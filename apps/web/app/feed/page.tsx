7d00cc9adeb3caf9de888dce8994af0070ee7613
Terris Kremer <terris.kremer@gmail.com>
2024-03-05 11:04:35 -0500

    some cleanup

--- apps/web/app/feed/page.tsx
+++ apps/web/app/feed/page.tsx
@@ -20,23 +20,25 @@ export default function FeedPage() {
         <Link
           key={adventureLog._id}
           href={`/adventure-logs/${adventureLog._id}`}
-          className="w-full border rounded p-4 pb-8 cursor-pointer hover:border-muted transition-all"
+          className="w-full border border-dashed rounded pb-8 cursor-pointer hover:border-muted transition-all"
         >
           <LogShowcaseImage
             showcaseFileId={adventureLog.showcaseFileId}
             className="pb-8"
           />
-          <Text className="text-center font-black text-xl pb-4">
-            {adventureLog.title}
-          </Text>
-          <hr className="w-[30px] mx-auto mb-4" />
-          <Text className="font-soleil text-center font-bold uppercase text-xs tracking-wider pb-2">
-            {adventureLog.location?.name}
-          </Text>
-          <Text className="text-center text-xs">
-            {adventureLog.location?.latitude},{" "}
-            {adventureLog.location?.longitude}
-          </Text>
+          <div className="p-4">
+            <Text className="text-center font-black text-xl pb-4">
+              {adventureLog.title}
+            </Text>
+            <hr className="w-[30px] mx-auto mb-4" />
+            <Text className="font-soleil text-center font-bold uppercase text-xs tracking-wider pb-2">
+              {adventureLog.location?.name}
+            </Text>
+            <Text className="text-center text-xs">
+              {adventureLog.location?.latitude},{" "}
+              {adventureLog.location?.longitude}
+            </Text>
+          </div>
         </Link>
       ))}
     </div>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,22 @@
-import { Text } from "@repo/ui";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useMeContext } from "@repo/auth/context";
+import { LoadingScreen, Text } from "@repo/ui";
 import { JoinWaitlistForm } from "./JoinWaitlistForm";
 
 export default function HomePage() {
+  const router = useRouter();
+  const { isAuthenticated, isLoading } = useMeContext();
+
+  useEffect(() => {
+    if (isLoading || !isAuthenticated) return;
+    router.push("/feed");
+  }, [isAuthenticated, isLoading, router]);
+
+  if (isLoading) return <LoadingScreen />;
+
   return (
     <div className="w-full p-8 pt-16">
       <div className="my-16 mx-auto md:w-[700px]">
