c1f7cd650b03dd466bb8371e0e45d17478b8e556
Terris Kremer <terris.kremer@gmail.com>
2024-04-27 15:17:41 -0400

    hide footer links to unauthorized users

--- apps/web/app/Footer.tsx
+++ apps/web/app/Footer.tsx
@@ -5,16 +5,22 @@ import { cn } from "@repo/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Text } from "@repo/ui";
+import { useMeContext } from "@repo/auth/context";
 
 export function Footer() {
+  const { isAuthenticated } = useMeContext();
   return (
     <div className="w-full border-t py-8 mt-8">
       <div className="container flex flex-col items-start justify-start md:flex-row">
-        <Text className="w-full p-2">Copyright &copy; 2024 Terris Kremer</Text>
-        <div className="w-full flex flex-col items-start justify-start md:flex-row md:justify-end md:gap-8">
-          <FooterLink href="/about">Charter</FooterLink>
-          <FooterLink href="/shop">Shop</FooterLink>
-        </div>
+        <Text className="w-full text-sm p-2">
+          Copyright &copy; 2024 Terris Kremer
+        </Text>
+        {isAuthenticated ? (
+          <div className="w-full flex flex-col items-start justify-start md:flex-row md:justify-end md:gap-8">
+            <FooterLink href="/about">Charter</FooterLink>
+            <FooterLink href="/shop">Shop</FooterLink>
+          </div>
+        ) : null}
       </div>
     </div>
   );
