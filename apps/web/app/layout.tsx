e3bdb5577a5be83186ddb8e767062600d7752467
Terris Kremer <terris.kremer@gmail.com>
2024-04-28 12:30:50 -0400

    add min height to main

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children, modal }: RootLayoutProps) {
         <AppProviders>
           <div className="flex flex-col w-full h-full min-h-screen flex-1">
             <Masthead />
-            <main className="w-full flex flex-col items-start justify-start">
+            <main className="w-full flex flex-col items-start justify-start min-h-[75vh]">
               {children}
             </main>
             <Footer />
