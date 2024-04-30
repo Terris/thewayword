61d3d0adc61f2720cfa820de2cbedb4ed8a90703
Terris Kremer <terris.kremer@gmail.com>
2024-04-30 08:40:13 -0400

    store page fix

--- apps/web/app/shop/page.tsx
+++ apps/web/app/shop/page.tsx
@@ -2,12 +2,10 @@ import { Text } from "@repo/ui";
 
 export default function AboutPage() {
   return (
-    <div className="w-full p-8">
-      <div className="container mx-auto text-lg">
-        <Text className="text-4xl font-black text-center">
-          The Wayword shop is coming soon.
-        </Text>
-      </div>
+    <div className="w-full p-8 pt-32">
+      <Text className="text-4xl font-black text-center">
+        The Wayword shop is coming soon.
+      </Text>
     </div>
   );
 }
