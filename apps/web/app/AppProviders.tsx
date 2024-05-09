49f41f81cafd10521f73a66648b87851ed4ced19
Terris Kremer <terris.kremer@gmail.com>
2024-05-09 01:25:27 -0400

    fix theme

--- apps/web/app/AppProviders.tsx
+++ apps/web/app/AppProviders.tsx
@@ -22,7 +22,7 @@ export function AppProviders({ children }: { children: React.ReactNode }) {
         <MeProvider>
           <NextThemesProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
           >
             <TooltipProvider>{children}</TooltipProvider>
