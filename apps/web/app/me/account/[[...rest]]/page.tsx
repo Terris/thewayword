5db83753a8cdb45638d139046f96d91d4f21b24b
Terris Kremer <terris.kremer@gmail.com>
2024-05-08 20:35:01 -0400

    account page style improvements

--- apps/web/app/me/account/[[...rest]]/page.tsx
+++ apps/web/app/me/account/[[...rest]]/page.tsx
@@ -3,21 +3,33 @@
 import { UserProfile } from "@clerk/nextjs";
 import { LoadingScreen } from "@repo/ui";
 import { useMeContext } from "@repo/auth/context";
+import { useMediaQuery } from "@repo/hooks";
 
 export default function AccountPage() {
   const { isLoading: authIsLoading, me } = useMeContext();
+  const isLargeWidth = useMediaQuery("(min-width: 768px)");
 
   if (!me || authIsLoading) return <LoadingScreen />;
 
   return (
-    <div className="mx-auto">
+    <div className="w-full mx-auto">
       <UserProfile
         appearance={{
           elements: {
+            rootBox: {
+              width: "100%",
+            },
             cardBox: {
+              width: "100%",
+              maxWidth: "100%",
               boxShadow: "none",
             },
-            navbar: { background: "#ffffff" },
+            navbar: {
+              background: "#ffffff",
+              width: isLargeWidth ? "25%" : "100%",
+              maxWidth: isLargeWidth ? "25%" : "100%",
+              flex: isLargeWidth ? "0 0 25%" : "0 0 100%",
+            },
             scrollBox: {
               borderRadius: "0",
               boxShadow: "none",
--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./use-debounce";
 export * from "./use-geolocation";
+export * from "./use-isomorphic-layout-effect";
 export * from "./use-lock-body-scroll";
+export * from "./use-mediaquery";
