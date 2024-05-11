6f91d1f4e55f5aa8ba3f423392321b241c9172c2
Terris Kremer <terris.kremer@gmail.com>
2024-05-10 20:27:01 -0400

    feed items let user click on user avatar separately

--- apps/web/app/_components/AdventureLogFeedItem.tsx
+++ apps/web/app/_components/AdventureLogFeedItem.tsx
@@ -12,35 +12,38 @@ export function AdventureLogFeedItem({
   adventureLog,
 }: AdventureLogFeedItemProps) {
   return (
-    <Link
-      href={`/adventure-logs/${adventureLog._id}`}
-      className="group w-full pb-4 cursor-pointer"
-    >
-      <div className="relative flex items-center justify-center bg-primary rounded">
-        <AdventureLogCoverImage
-          adventureLogId={adventureLog._id}
-          className="group-hover:opacity-50 transition-opacity"
-        />
-      </div>
-      <div className="py-2 flex flex-col justify-start items-start">
+    <div>
+      <Link
+        href={`/adventure-logs/${adventureLog._id}`}
+        className="group w-full pb-4 cursor-pointer"
+      >
+        <div className="relative flex items-center justify-center bg-primary rounded mb-2">
+          <AdventureLogCoverImage
+            adventureLogId={adventureLog._id}
+            className="group-hover:opacity-50 transition-opacity"
+          />
+        </div>
         <Text className="w-full font-soleil font-bold uppercase text-xs tracking-wider truncate pb-1">
           {adventureLog.title}
         </Text>
-        <div className="flex items-center">
-          {adventureLog.user.avatarUrl ? (
-            <Image
-              src={adventureLog.user.avatarUrl}
-              width="20"
-              height="20"
-              alt="User"
-              className="w-5 h-5 rounded-full mr-2"
-            />
-          ) : null}
-          <Text className="text-sm text-center leading-none italic">
-            {adventureLog.user.name}
-          </Text>
-        </div>
-      </div>
-    </Link>
+      </Link>
+      <Link
+        href={`/user/${adventureLog.userId}/adventure-logs`}
+        className="group flex items-center cursor-pointer"
+      >
+        {adventureLog.user.avatarUrl ? (
+          <Image
+            src={adventureLog.user.avatarUrl}
+            width="20"
+            height="20"
+            alt="User"
+            className="w-5 h-5 rounded-full mr-2"
+          />
+        ) : null}
+        <Text className="text-sm text-center leading-none italic group-hover:underline">
+          {adventureLog.user.name}
+        </Text>
+      </Link>
+    </div>
   );
 }
