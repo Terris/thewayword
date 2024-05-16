d733ce17785f5cc75b879a50cdb5830748208fc1
Terris Kremer <terris.kremer@gmail.com>
2024-05-16 16:52:15 -0400

    Update page.tsx

--- apps/web/app/shop/page.tsx
+++ apps/web/app/shop/page.tsx
@@ -13,28 +13,28 @@ export default function ShopPage() {
   // const isLoading = allProducts === undefined;
 
   return (
-      <div className="w-full p-8 ">
-        <Text className="text-2xl font-bold text-center">
-          The Wayword Shop is coming soon!
-        </Text>
-        <Text className="text-center pb-16">
-          ...but here&rsquo;s a preview of what&rsquo;s <i>in store</i>.
-        </Text>
-        <div className="container grid grid-cols-1 md:grid-cols-3 gap-4">
-          {allProducts?.map((product) => (
-            <div className="group" key={product._id}>
-              <Link href={`/shop/products/${product._id}`}>
-                <ShopProductCoverImage shopProductId={product._id} />
-                <Text className="text-lg font-bold text-center pr-4 pt-8">
-                  {product.name}
-                </Text>
-                <Text className="text-center pr-4 italic text-gray-500">
-                  ${product.priceInCents / 100}
-                </Text>
-              </Link>
-            </div>
-          ))}
-        </div>
+    <div className="w-full p-8 ">
+      <Text className="text-2xl font-bold text-center">
+        The Wayword Shop is coming soon!
+      </Text>
+      <Text className="text-center pb-16">
+        ...but here&rsquo;s a preview of what&rsquo;s <i>in store</i>.
+      </Text>
+      <div className="container grid grid-cols-1 md:grid-cols-3 gap-4">
+        {allProducts?.map((product) => (
+          <div className="group" key={product._id}>
+            <Link href={`/shop/products/${product._id}`}>
+              <ShopProductCoverImage shopProductId={product._id} />
+              <Text className="text-lg font-bold text-center pr-4 pt-8">
+                {product.name}
+              </Text>
+              <Text className="text-center pr-4 italic text-gray-500">
+                ${product.priceInCents / 100}
+              </Text>
+            </Link>
+          </div>
+        ))}
       </div>
+    </div>
   );
 }
