6565289a0e1f6ab9387fd286d46c420efcd5f1c0
Terris Kremer <terris.kremer@gmail.com>
2024-05-20 14:11:26 -0400

    revert to native date picker input

--- packages/ui/src/DatePicker.tsx
+++ packages/ui/src/DatePicker.tsx
@@ -1,12 +1,4 @@
-"use client";
-
-import { useState } from "react";
-import { format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
-import { cn } from "@repo/utils";
-import { Button } from "./Button";
-import { Calendar } from "./Calendar";
-import { Popover, PopoverContent, PopoverTrigger } from "./Popover";
+import { Input } from "./Input";
 
 export function DatePicker({
   dateAsISOString,
@@ -14,41 +6,19 @@ export function DatePicker({
   placeholder = "Pick a date",
 }: {
   dateAsISOString: string | undefined;
-  setDate: (dateAsIsoString: string | undefined) => void;
+  setDate: (dateAsString: string | undefined) => void;
   placeholder?: string;
 }) {
-  const [open, setOpen] = useState(false);
-  const date = dateAsISOString ? new Date(dateAsISOString) : undefined;
   return (
-    <Popover
-      open={open}
-      onOpenChange={(o) => {
-        setOpen(o);
-      }}
-    >
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          className={cn(
-            "w-full justify-start text-left font-normal",
-            !date && "text-muted-foreground"
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>{placeholder}</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={(d) => {
-            setDate(d?.toISOString());
-            setOpen(false);
-          }}
-          initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+    <div className="flex flex-row gap-2">
+      <Input
+        value={dateAsISOString}
+        type="date"
+        placeholder={placeholder}
+        onChange={(e) => {
+          setDate(e.currentTarget.value);
+        }}
+      />
+    </div>
   );
 }
