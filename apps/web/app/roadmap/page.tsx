aab5ec5039758301763954d6af9828b5669ee85c
Terris Kremer <terris.kremer@gmail.com>
2024-05-20 18:51:32 -0400

    update roadmap

--- apps/web/app/roadmap/page.tsx
+++ apps/web/app/roadmap/page.tsx
@@ -12,50 +12,22 @@ export default function RoadmapPage() {
         I&rsquo;m working on.
       </Text>
       <Text className="font-soleil font-bold uppercase tracking-wide pb-4">
-        Major Features
+        Planned Features
       </Text>
       <ul className="pb-8 list-disc pl-5">
         <li>Users can bucket list a log</li>
         <li>Users can include other users in a log</li>
         <li>Users can add gallery image blocks</li>
-        <li>Users can caption an image block</li>
         <li>Users can create detailed maps</li>
         <li>Users can embed videos</li>
         <li>Users can print / download adventure logs</li>
         <li>Users can purchase printed physical adventure logs</li>
         <li>Users can download a social media kit</li>
         <li>Users can view a map of all adventure logs</li>
-        <li className="line-through">Users can follow other users</li>
-        <li className="line-through">Users can shop</li>
-      </ul>
-      <Text className="font-soleil font-bold uppercase tracking-wide pb-4">
-        Quality of life
-      </Text>
-      <ul className="list-disc pl-5">
         <li>
           User can click a location and see all logs with the same location
         </li>
         <li>User can turn off comments for their own logs</li>
-        <li className="line-through">
-          Improve date selection: user should be able to go way back in time
-          faster
-        </li>
-        <li className="line-through">
-          Improve date selection: re-opening the cal after selecting a date
-          should automatically show the previously selected date
-        </li>
-        <li className="line-through">
-          User can click on a tag and see logs with a similar tag
-        </li>
-        <li className="line-through">Add text formatting to story blocks</li>
-        <li className="line-through">
-          Add an alert box in the header that shows a list of events (e.g.
-          someone commented on my post)
-        </li>
-        <li className="line-through">
-          User can select a display size on image blocks
-        </li>
-        <li className="line-through">User can delete their own logs</li>
       </ul>
     </div>
   );
