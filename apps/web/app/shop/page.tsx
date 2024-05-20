4b876bb5af72a729f32e92e9653a7f67671265f3
Terris Kremer <terris.kremer@gmail.com>
2024-05-20 14:55:32 -0400

    add a no products message to empty shop page

--- apps/web/app/shop/page.tsx
+++ apps/web/app/shop/page.tsx
@@ -13,21 +13,27 @@ export default function ShopPage() {
 
   return (
     <div className="w-full p-8 ">
-      <div className="container grid grid-cols-1 md:grid-cols-3 gap-4">
-        {allProducts?.map((product) => (
-          <div className="group" key={product._id}>
-            <Link href={`/shop/products/${product._id}`}>
-              <ShopProductCoverImage shopProductId={product._id} />
-              <Text className="text-lg font-bold text-center pr-4 pt-8">
-                {product.name}
-              </Text>
-              <Text className="text-center pr-4 italic text-gray-500">
-                ${product.priceInCents / 100}
-              </Text>
-            </Link>
-          </div>
-        ))}
-      </div>
+      {allProducts?.length ? (
+        <div className="container grid grid-cols-1 md:grid-cols-3 gap-4">
+          {allProducts.map((product) => (
+            <div className="group" key={product._id}>
+              <Link href={`/shop/products/${product._id}`}>
+                <ShopProductCoverImage shopProductId={product._id} />
+                <Text className="text-lg font-bold text-center pr-4 pt-8">
+                  {product.name}
+                </Text>
+                <Text className="text-center pr-4 italic text-gray-500">
+                  ${product.priceInCents / 100}
+                </Text>
+              </Link>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <Text className="text-center py-16">
+          Ohp, sorry! There are no products available at this time.
+        </Text>
+      )}
     </div>
   );
 }
