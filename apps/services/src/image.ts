774098a40c4c00fed8ad3b2dedee341fee352d9c
Terris Kremer <terris.kremer@gmail.com>
2024-05-23 16:08:06 -0400

    fix for image optim rotation

--- apps/services/src/image.ts
+++ apps/services/src/image.ts
@@ -19,6 +19,7 @@ export async function optimizeFileImage(req: Request, res: Response) {
       fit: "inside",
       withoutEnlargement: true,
     })
+    .withMetadata()
     .toFormat("webp")
     .toBuffer();
 
