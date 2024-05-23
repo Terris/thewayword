8b0163465edaeb15209df5e4d007dc1c42248516
Terris Kremer <terris.kremer@gmail.com>
2024-05-23 16:18:54 -0400

    add fix rotated images migration

--- packages/convex/src/migrations.ts
+++ packages/convex/src/migrations.ts
@@ -2,25 +2,26 @@ import { asyncMap } from "convex-helpers";
 import { internal } from "./_generated/api";
 import { internalMutation } from "./_generated/server";
 
-export const optimizeAllImages = internalMutation({
+export const fixRotatedImages = internalMutation({
   args: {},
   handler: async (ctx) => {
+    const fuckedFileIds = [
+      "m175zvrg1nhbbhx641b0kn61a96ry7xt",
+      "m17c1a328wbyxjzam8wrswdaas6sdx19",
+      "m1749tqf71xgvxe2y0mp6gkmp96rrftc",
+      "m17d797vff871tmshv0srrrt5d6rr2n5",
+      "m17fr7m57x16t0dhatscv07xbx6rsdw6",
+      "m175vxybapcvpczpccjywh2zp96s92xy",
+    ];
+
     const files = await ctx.db.query("files").collect();
-    await asyncMap(files, async (file) => {
-      if (!file.originalUrl || file.originalUrl === file.url) {
-        if (!file.originalUrl) {
-          await ctx.db.patch(file._id, {
-            originalUrl: file.url,
-          });
-        }
-        await ctx.scheduler.runAfter(
-          0,
-          internal.optimizerActions.optimizeImage,
-          {
-            fileId: file._id,
-          }
-        );
-      }
+
+    const filesToFix = files.filter((file) => fuckedFileIds.includes(file._id));
+
+    await asyncMap(filesToFix, async (file) => {
+      await ctx.scheduler.runAfter(0, internal.optimizerActions.optimizeImage, {
+        fileId: file._id,
+      });
     });
     return true;
   },
