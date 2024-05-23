40c1c7d1f4432a7fac082c23c2c82f7bb5f4470a
Terris Kremer <terris.kremer@gmail.com>
2024-05-23 04:59:45 -0400

    fix log likes

--- apps/web/app/_components/AdventureLogLikes.tsx
+++ apps/web/app/_components/AdventureLogLikes.tsx
@@ -17,23 +17,25 @@ export function AdventureLogLikes({
     <div className="w-full" id="likes">
       <Text className="text-xl font-bold pb-2">Likes</Text>
       <hr className="border-b-1 border-dashed mb-4" />
+
       {likes?.map((like) => (
-        <Link
-          href={`/user/${like.userId}/adventure-logs`}
-          className="inline-flex flex-row items-center gap-2 pb-4 text-sm hover:text-amber-400"
-          key={like._id}
-        >
-          {like.user.avatarUrl ? (
-            <Image
-              src={like.user.avatarUrl}
-              width="20"
-              height="20"
-              alt="User"
-              className="w-5 h-5 rounded-full"
-            />
-          ) : null}
-          <Text>{like.user.name}</Text>
-        </Link>
+        <div key={like._id}>
+          <Link
+            href={`/user/${like.userId}/adventure-logs`}
+            className="inline-flex flex-row items-center gap-2 pb-4 text-sm hover:text-amber-400"
+          >
+            {like.user.avatarUrl ? (
+              <Image
+                src={like.user.avatarUrl}
+                width="20"
+                height="20"
+                alt="User"
+                className="w-5 h-5 rounded-full"
+              />
+            ) : null}
+            <Text>{like.user.name}</Text>
+          </Link>
+        </div>
       ))}
     </div>
   );
