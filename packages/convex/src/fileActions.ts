ab1278b52f46df3837638a2833d923c4c48a80e9
Terris Kremer <terris.kremer@gmail.com>
2024-05-25 22:15:07 -0400

    add back cleanup to prune file records

--- packages/convex/src/fileActions.ts
+++ packages/convex/src/fileActions.ts
@@ -6,6 +6,7 @@ import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 import { ConvexError, v } from "convex/values";
 import { internal } from "./_generated/api";
 import { internalAction } from "./_generated/server";
+import { asyncMap } from "convex-helpers";
 
 export const systemDeleteFileAndS3ObjectsById = internalAction({
   args: { id: v.id("files") },
@@ -46,6 +47,45 @@ export const systemDeleteFileAndS3ObjectsById = internalAction({
     // delete file record
     await ctx.runMutation(internal.files.systemDeleteById, {
       id: file._id,
+    });
+    return true;
+  },
+});
+
+export const systemCleanUpFiles = internalAction({
+  args: {},
+  handler: async (ctx) => {
+    const allFiles = await ctx.runQuery(internal.files.systemFindAll);
+
+    await asyncMap(allFiles, async (file) => {
+      // figure out if the file is used anywhere
+      // is it part of an adventureLogBlock
+      const relatedAdventureLogBlock = await ctx.runQuery(
+        internal.adventureLogBlocks.systemFindByFileId,
+        {
+          fileId: file._id,
+        }
+      );
+      if (relatedAdventureLogBlock) return;
+
+      // or is it part of a shopProductImage
+      const relatedShopProductImage = await ctx.runQuery(
+        internal.shopProductImages.systemFindByFileId,
+        {
+          fileId: file._id,
+        }
+      );
+      if (relatedShopProductImage) return;
+
+      // if not, delete the s3 files and the file record
+      await ctx.runAction(
+        internal.fileActions.systemDeleteFileAndS3ObjectsById,
+        {
+          id: file._id,
+        }
+      );
+
+      return true;
     });
     return true;
   },
