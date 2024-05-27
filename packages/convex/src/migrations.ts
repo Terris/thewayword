f1092f55ea496bec3810aaa1ac9c62c7b8ca3293
Terris Kremer <terris.kremer@gmail.com>
2024-05-27 03:21:06 -0400

    update content

--- packages/convex/src/migrations.ts
+++ packages/convex/src/migrations.ts
@@ -4,12 +4,6 @@ import { internal } from "./_generated/api";
 
 export const indexSearchContentForAllAdventureLogs = internalMutation({
   handler: async (ctx) => {
-    const adventureLogs = await ctx.db.query("adventureLogs").collect();
-    await asyncMap(adventureLogs, async (adventureLog) => {
-      ctx.db.patch(adventureLog._id, {
-        indexableContentUpdatedAt: new Date().toISOString(),
-      });
-    });
     await ctx.scheduler.runAfter(
       0,
       internal.adventureLogs.systemIndexSearchContent,
